a7d2ef7fc9872b8561e69472476404a9a000c8bc
Jiarong Qiu <qiujiaro@usc.edu>
2026-04-20 14:51:59 -0700

    feat: recreate_client

---
+++ src/recreate_client.py
@@ -0,0 +1,16 @@
+
+from schemas import SCHEMAS
+def recreate_keyspace(session, rf=2, KEYSPACE="test"):
+    session.execute(f"DROP KEYSPACE IF EXISTS {KEYSPACE}")
+    session.execute(f"""
+        CREATE KEYSPACE {KEYSPACE}
+        WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': {rf}}}
+    """)
+    session.set_keyspace(KEYSPACE)
+
+
+def recreate_schema(session, schema_name):
+    schema = SCHEMAS[schema_name]
+    session.execute(schema["drop"])
+    session.execute(schema["create"])
+    print(f"Created schema: {schema_name}")
